5da86390db2f32acb8a09c0e0f7e0468f30a2c04
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-13 20:49:40 -0500

    feat: Add HeroesRoutes component for routing

---
+++ src/heroes/routes/HeroesRoutes.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../components/Navbar';
+import { DCPage } from '../pages/DCPage';
+import { HeroPage } from '../pages/HeroPage';
+import { MarvelPage } from '../pages/MarvelPage';
+import { SearchPage } from '../pages/SearchPage';
+
+export const HeroesRoutes = (): JSX.Element => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="marvel" element={<MarvelPage />} />
+          <Route path="dc" element={<DCPage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="hero" element={<HeroPage />} />
+          <Route path="/" element={<Navigate to="/marvel" />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
