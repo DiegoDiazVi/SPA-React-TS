afcaae35874ee1df0d81369349e82ee6ccdc1a1e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-15 18:00:21 -0500

    feat: Add utility function to get heroes by publisher

---
+++ src/heroes/utils/getHeroesByPublisher.ts
@@ -0,0 +1,11 @@
+import type { Hero, Publisher } from '../../types/types';
+import { heroes } from '../data/heroes';
+
+export const getDataByPublisher = (publisher: Publisher): Hero[] => {
+  const validPublishers: Publisher[] = ['DC Comics', 'Marvel Comics'];
+
+  if (!validPublishers.includes(publisher)) {
+    throw new Error(`Publisher "${publisher}" is not valid`);
+  }
+  return heroes.filter((hero) => hero.publisher === publisher);
+};
