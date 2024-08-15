852201aa7c7fd6963b80cbc44532671d99b498f9
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-15 18:12:19 -0500

    feat: Update DCPage and MarvelPage with HeroList component

--- src/heroes/pages/DCPage.tsx
+++ src/heroes/pages/DCPage.tsx
@@ -1,7 +1,12 @@
+import { HeroList } from '../components/HeroList';
+
 export const DCPage = (): JSX.Element => {
   return (
     <>
-      <h1>DCPage</h1>
+      <h2 className="mt-5">DC Page</h2>
+      <hr />
+
+      <HeroList publisher={'DC Comics'} />
     </>
   );
 };
--- src/heroes/pages/MarvelPage.tsx
+++ src/heroes/pages/MarvelPage.tsx
@@ -1,7 +1,12 @@
+import { HeroList } from '../components/HeroList';
+
 export const MarvelPage = (): JSX.Element => {
   return (
     <>
-      <h1>MarvelPage</h1>
+      <h2 className="mt-5">MarvelPage</h2>
+      <hr />
+
+      <HeroList publisher={'Marvel Comics'} />
     </>
   );
 };
