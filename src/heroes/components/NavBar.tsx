b7c9eb25883cfb605511f5ed320299ecc7fbdfa8
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-15 17:38:51 -0500

    feat: Update NavBar component to navigate to login page

--- src/heroes/components/NavBar.tsx
+++ src/heroes/components/NavBar.tsx
@@ -1,8 +1,20 @@
-import { Link, NavLink, type NavLinkRenderProps } from 'react-router-dom';
+import {
+  Link,
+  NavLink,
+  useNavigate,
+  type NavLinkRenderProps,
+  type NavigateFunction,
+} from 'react-router-dom';
 
 export const Navbar = (): JSX.Element => {
+  const navigate: NavigateFunction = useNavigate();
+
   const getNavLinkClass = ({ isActive }: NavLinkRenderProps): string => {
     return `nav-item nav-link ${isActive ? 'active' : ''} `;
+  };
+
+  const handleLogout = (): void => {
+    navigate('/login');
   };
 
   return (
@@ -32,7 +44,9 @@ export const Navbar = (): JSX.Element => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Diego</span>
-          <button className="nav-item nav-link btn">Logout</button>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>
