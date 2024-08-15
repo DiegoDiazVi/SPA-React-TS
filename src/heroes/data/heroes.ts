1e982a3d2817e55fc2b2a81d2d6fb741515fc3cb
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-15 18:00:13 -0500

    feat: Import hero types and update heroes data

--- src/heroes/data/heroes.ts
+++ src/heroes/data/heroes.ts
@@ -1,11 +1,4 @@
-interface Hero {
-  id: string;
-  superhero: string;
-  publisher: string;
-  alter_ego: string;
-  first_appearance: string;
-  characters: string;
-}
+import type { Hero } from '../../types/types';
 
 export const heroes: Hero[] = [
   {
