7f9d1c89bc6db372370b2633692c8231d140d21c
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-21 18:54:07 -0500

    feat: Wrap App component with AuthProvider for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { AuthProviver } from './auth/context/AuthContext';
 import { AppRouter } from './router/AppRouter';
 
 function App(): JSX.Element {
   return (
-    <>
+    <AuthProviver>
       <AppRouter />
-    </>
+    </AuthProviver>
   );
 }
 
