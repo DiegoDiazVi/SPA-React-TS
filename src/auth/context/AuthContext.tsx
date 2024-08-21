9c826e52ccee95b782495fc6a93c45572e8cd20e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-21 18:51:14 -0500

    refactor: Update AuthContext to use useReducer for state management

--- src/auth/context/AuthContext.tsx
+++ src/auth/context/AuthContext.tsx
@@ -1,14 +1,23 @@
 import { createContext, useReducer } from 'react';
-import type { AuthProviderProps } from '../../types/types';
+import type {
+  AuthContextType,
+  AuthProviderProps,
+  AuthState,
+} from '../../types/types';
 import { authReducer } from './authReducer';
 
 export const AuthContext = createContext<AuthContextType | null>(null);
-const initialAuthState = {
+
+const initialAuthState: AuthState = {
   logged: false,
   name: '',
 };
-const [authState, dispatch] = useReducer(authReducer, initialAuthState);
 
 export const AuthProviver = ({ children }: AuthProviderProps): JSX.Element => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [authState, dispatch] = useReducer(authReducer, initialAuthState);
+  return (
+    <AuthContext.Provider value={{ authState, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
