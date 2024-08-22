f749186125136719f97ca0074cca9969aaa058ad
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-21 21:10:55 -0500

    feat: Update AppRouter to use PublicRoutes for login route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,11 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 import { PrivateRoutes } from './PrivateRoutes';
+import { PublicRoutes } from './PublicRoutes';
 
 export const AppRouter = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="login" element={<LoginPage />} />
+      <Route
+        path="login"
+        element={
+          <PublicRoutes>
+            <LoginPage />
+          </PublicRoutes>
+        }
+      />
       <Route
         path="/*"
         element={
