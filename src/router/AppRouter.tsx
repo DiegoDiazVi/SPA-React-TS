30aece68b6d08803f52154d7437e54c5872791f7
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-21 21:01:53 -0500

    feat: Add PrivateRoutes component for authenticated routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoutes } from './PrivateRoutes';
 
 export const AppRouter = (): JSX.Element => {
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
-      <Route path="/*" element={<HeroesRoutes />} />
+      <Route
+        path="/*"
+        element={
+          <PrivateRoutes>
+            <HeroesRoutes />
+          </PrivateRoutes>
+        }
+      />
     </Routes>
   );
 };
